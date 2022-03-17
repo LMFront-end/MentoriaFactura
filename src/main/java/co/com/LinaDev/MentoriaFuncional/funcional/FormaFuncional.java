36bdf94d4f51e0879338ad41a142df10b44eef1a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-16 20:46:15 -0500

    Se crea lista Factura

--- src/main/java/co/com/LinaDev/MentoriaFuncional/funcional/FormaFuncional.java
+++ src/main/java/co/com/LinaDev/MentoriaFuncional/funcional/FormaFuncional.java
@@ -1,6 +1,9 @@
 package co.com.LinaDev.MentoriaFuncional.funcional;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FormaFuncional {
 
     public static void main(String[] args) {
@@ -14,7 +17,17 @@ public class FormaFuncional {
         Factura f6 = new Factura(6, "Espejo", 750, new MyDate("2018-05-05"), DescuentoValue.NO_APLICA);
         Factura f7 = new Factura(7, "Aro de luz", 200, new MyDate("2018-05-05"), DescuentoValue.NO_APLICA);
 
-        
+        // generar una lista
+        List<Factura> lista = new ArrayList<>();
+
+        // agregar los productos de la factura
+        lista.add(f1);
+        lista.add(f2);
+        lista.add(f3);
+        lista.add(f4);
+        lista.add(f5);
+        lista.add(f6);
+        lista.add(f7);
 
     }
 }
